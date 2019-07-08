e39f2f5c3d7e03d8c815457374650ec7af10e8d5
rafiq-istvn <rafiq.istvn@gmail.com>
2019-07-08 16:03:49 +0700

    Spring Login Template Added, Spring Security 5

--- src/main/java/com/example/demo/LoginDemoApplication.java
+++ src/main/java/com/example/demo/LoginDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginDemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable{
 		SpringApplication.run(LoginDemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/demo/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/**
+ * Must need spring 5 to implements this.
+ * here use version is -> 2.1.6.RELEASE
+ * @author MHC
+ *
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer{
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
